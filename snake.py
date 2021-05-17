27e5176ebc21efda341d02164552b8ccb0df8b34
Vinicius Perillo <vinicius.perillo25@gmail.com>
2021-05-17 13:37:06 -0300

    Geração da comida e alimentação da cobra
    Criado as funções que geram e que fazem a cobra comer a comida, além disso houve a diminuição do mapa.

--- snake.py
+++ snake.py
@@ -1,11 +1,12 @@
 from time import sleep
 from os import system, name
+from random import randint
 import keyboard
 
 #Variables
 snake_map = []
 snake_body = []
-
+food = {}
 direction = {
     'x': 0,
     'y': 0,
@@ -19,17 +20,20 @@ def arrow_up_event(evt):
         direction['y'] = -1
         direction['is_changed'] = True
 
+
 def arrow_down_event(evt):
     if(direction['y'] != -1 and not direction['is_changed']):
         direction['x'] = 0
         direction['y'] = 1
         direction['is_changed'] = True
 
+
 def arrow_left_event(evt):
     if(direction['x'] != 1 and not direction['is_changed']):
         direction['x'] = -1
         direction['y'] = 0
         direction['is_changed'] = True
+
 
 def arrow_right_event(evt):
     if(direction['x'] != -1 and not direction['is_changed']):
@@ -49,25 +53,30 @@ def constructor():
 
 
 def frame():
-    system('cls' if name == 'nt' else 'clear')
-    #clear_snake_map()
     move_snake()
+    eat_food()
+    system('cls' if name == 'nt' else 'clear')
     print(snake_map_str())
-    print('{:^62}'.format('Segure ESC para sair'))
-    sleep(0.5)
-    direction['is_changed'] = False
+    print('{:^42}'.format('Segure ESC para sair'))
+    sleep(0.2)
+    
 
 
 def create_snake_map():
-    for i in range(0,31):
+    for i in range(21):
         matrix_column = []
-        for j in range(0,31):
-            if(i == 0 or j == 0 or i == 30 or j == 30 ):
-                matrix_column.append('x ')
-            else:            
-                matrix_column.append('  ')
+        for j in range(21):            
+                matrix_column.append('')
         snake_map.append(matrix_column)
 
+
+def clean_snake_map():
+    for j in range(21):
+        for i in range(21):
+            if(i == 0 or j == 0 or i == 20 or j == 20 ):
+                snake_map[j][i] = 'x '
+            else:
+                snake_map[j][i] = '  '
 
 def snake_map_str():
     map_str = ''
@@ -79,7 +88,7 @@ def snake_map_str():
 
 
 def reset_snake_body():
-    for c in range(16,13,-1):
+    for c in range(11,8,-1):
         snake_body.append({
             'x': c,
             'y': 15
@@ -87,15 +96,22 @@ def reset_snake_body():
         write_snake_body()
 
 
+def spawn_snake_body(x, y):
+    snake_body.append({
+        'x': x,
+        'y': y
+    })
+
 def write_snake_body():
     for sb in snake_body:
         snake_map[sb['y']][sb['x']] = 'o '
 
 
 def erase_snake_body():
-    for y in range(31):
-        for x in range(31):
+    for y in range(21):
+        for x in range(21):
             snake_map[y][x] = snake_map[y][x].replace('o', ' ')
+
 
 def move_snake():
     erase_snake_body()
@@ -106,12 +122,27 @@ def move_snake():
         else:
             snake_body[index] = snake_body[index-1].copy()
     write_snake_body()
+    direction['is_changed'] = False
 
+
+def spawn_food():
+    food['x'] = randint(1,19)
+    food['y'] = randint(1,19)
+    for sb in snake_body:
+        if(sb['x'] == food['x'] and sb['y'] == food['y']):
+            spawn_food()
+    snake_map[food['y']][food['x']] = 'f '
+
+def eat_food():
+    if snake_body[0] == food:
+        spawn_snake_body(food['x'], food['y'])
+        spawn_food()
 
 #Código principal
 constructor()
+spawn_food()
 print(snake_map_str())
-print('{:^62}'.format('Pressione qualquer seta para começar'))
+print('{:^42}'.format('Pressione qualquer seta para começar'))
 while not direction['is_changed']:
     pass
 while not keyboard.is_pressed('esc'):
